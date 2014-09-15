38497b954ebc77d6928f7366ba0fe92fce167f3b
aicike <176534021@qq.com>
2014-09-15 15:53:02 +0800

    Merge remote-tracking branch 'origin/gggmy' into wsq3
    Conflicts:
    	Web/Web.Publish.xml

---
+++ EF/Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using EF.Poco;
+namespace EF
+{
+    public class Context : DbContext
+    {
+        public DbSet<User> User{ get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+
+        }
+    }
+}
---
+++ EF/Migrations/201409150742304_Update.cs
@@ -0,0 +1,25 @@
+namespace EF.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Update : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.User",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.User");
+        }
+    }
+}
